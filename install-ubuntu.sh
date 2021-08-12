f48a0f6c3ee7ec35dc09464563edae494375b544
Rizki Amr <48115312+rizkiamr@users.noreply.github.com>
2021-08-13 02:39:26 +0700

    Create install-ubuntu.sh

---
+++ install-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+/etc/init.d/docker start
+docker version
